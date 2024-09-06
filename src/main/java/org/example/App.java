697e483f4107f2b4b2a59b2f8c7e129b4ddfe07a
voffie <viktorsoderborg@gmail.com>
2024-09-06 14:13:27 +0200

    Updates PriceData to use hour as identifier

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,34 +2,35 @@ package org.example;
 
 import java.util.*;
 
-record PriceData(String date, int price) {}
+record PriceData(String hour, int price) {
+}
 
 public class App {
     static List<PriceData> data = new ArrayList<>() {{
         add(new PriceData("00-01", 0));
         add(new PriceData("01-02", 0));
         add(new PriceData("02-03", 0));
-        add(new PriceData("03-04",0));
-        add(new PriceData("04-05",0));
-        add(new PriceData("05-06",0));
-        add(new PriceData("06-07",0));
-        add(new PriceData("07-08",0));
-        add(new PriceData("08-09",0));
-        add(new PriceData("09-10",0));
-        add(new PriceData("10-11",0));
-        add(new PriceData("11-12",0));
-        add(new PriceData("12-13",0));
-        add(new PriceData("13-14",0));
-        add(new PriceData("14-15",0));
-        add(new PriceData("15-16",0));
-        add(new PriceData("16-17",0));
-        add(new PriceData("17-18",0));
-        add(new PriceData("18-19",0));
-        add(new PriceData("19-20",0));
-        add(new PriceData("20-21",0));
-        add(new PriceData("21-22",0));
-        add(new PriceData("22-23",0));
-        add(new PriceData("23-24",0));
+        add(new PriceData("03-04", 0));
+        add(new PriceData("04-05", 0));
+        add(new PriceData("05-06", 0));
+        add(new PriceData("06-07", 0));
+        add(new PriceData("07-08", 0));
+        add(new PriceData("08-09", 0));
+        add(new PriceData("09-10", 0));
+        add(new PriceData("10-11", 0));
+        add(new PriceData("11-12", 0));
+        add(new PriceData("12-13", 0));
+        add(new PriceData("13-14", 0));
+        add(new PriceData("14-15", 0));
+        add(new PriceData("15-16", 0));
+        add(new PriceData("16-17", 0));
+        add(new PriceData("17-18", 0));
+        add(new PriceData("18-19", 0));
+        add(new PriceData("19-20", 0));
+        add(new PriceData("20-21", 0));
+        add(new PriceData("21-22", 0));
+        add(new PriceData("22-23", 0));
+        add(new PriceData("23-24", 0));
     }};
 
     public static void main(String[] args) {
@@ -37,15 +38,15 @@ public class App {
         boolean exit = false;
         while (!exit) {
             System.out.print("""
-                Elpriser
-                ========
-                1. Inmatning
-                2. Min, Max och Medel
-                3. Sortera
-                4. Bästa Laddningstid (4h)
-                5. Visualisering
-                e. Avsluta
-                """);
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    5. Visualisering
+                    e. Avsluta
+                    """);
             String action = scanner.nextLine();
 
             switch (action) {
@@ -60,9 +61,10 @@ public class App {
     }
 
     public static void addData(Scanner scanner) {
-        data.replaceAll(priceData -> new PriceData(priceData.date(), scanner.nextInt()));
+        data.replaceAll(priceData -> new PriceData(priceData.hour(), scanner.nextInt()));
         scanner.nextLine();
     }
+
     public static void minMaxAverage() {
         PriceData highest = Collections.max(data, Comparator.comparingInt(PriceData::price));
         PriceData lowest = Collections.min(data, Comparator.comparingInt(PriceData::price));
@@ -72,15 +74,17 @@ public class App {
                 Lägsta pris: %s, %s öre/kWh
                 Högsta pris: %s, %s öre/kWh
                 Medelpris: %.2f öre/kWh
-                """, lowest.date(), lowest.price(), highest.date(), highest.price(), average);
+                """, lowest.hour(), lowest.price(), highest.hour(), highest.price(), average);
     }
+
     public static void sortData() {
         List<PriceData> sorted = new ArrayList<>(data);
         sorted.sort(Comparator.comparingInt(PriceData::price).reversed());
         for (PriceData data : sorted) {
-            System.out.print(data.date() + " " + data.price() + " öre\n");
+            System.out.print(data.hour() + " " + data.price() + " öre\n");
         }
     }
+
     public static void bestCharge() {
         List<PriceData> sorted = new ArrayList<>(data);
         sorted.sort(Comparator.comparingInt(PriceData::price));
@@ -89,8 +93,9 @@ public class App {
         System.out.printf("""
                 Påbörja laddning klockan %s
                 Medelpris 4h: %.1f öre/kWh
-                """, sorted.getFirst().date().substring(0, 2), average);
+                """, sorted.getFirst().hour().substring(0, 2), average);
     }
+
     public static void visualizeData() {
         System.out.println("visualize data");
     }
